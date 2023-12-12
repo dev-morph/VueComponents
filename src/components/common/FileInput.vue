c31bdbdacfef83cebc8a9d9097079cc1a3d1e03b
htlee <morphlee@autocrypt.io>
2023-12-12 13:58:35 +0900

    FileInput 컴포넌트 추가

---
+++ src/components/common/FileInput.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="file__input__wrapper">
+        <label
+            :for="uuid"
+            @dragover.prevent
+            @dragenter.prevent
+            @drop.prevent="fileUploadHandler"
+            class="fi__label"
+        >
+            <span>{{ buttonText || "파일선택" }}</span>
+            <p> {{ !file || file.name.length <= 0 ? (placeholder || "파일을 선택하거나 이곳으로 드래그 해주세요.") : file.name }} </p>
+            <input :id="uuid" type='file' title="min is " placeholder="asdfasdf" @change="(e) => fileUploadHandler(e)"/>
+        </label>
+        <i v-if="fileName" @click.stop.prevent="(e) => fileUploadHandler(e)"></i>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface FileInputProps{
+    file: File | null
+    buttonText?: string
+    placeholder?: string
+}
+import { onMounted, ref, toRefs, computed } from 'vue';
+
+const props = defineProps<FileInputProps>()
+const { file, buttonText, placeholder} = toRefs(props);
+const emit = defineEmits(["update:modelValue", "test"]);
+// const uploadedFile = ref('');
+const uuid = crypto.randomUUID();
+
+const fileName = computed(() => {
+    if (file.value) {
+        return file.value.name;
+    }
+    return null;
+})
+
+function fileUploadHandler(e: Event) {
+    let files: FileList | null = null;
+    if (e instanceof DragEvent) {
+        if (e.dataTransfer) {
+            files = e.dataTransfer.files;
+        }
+    } else if (e instanceof MouseEvent) {
+        //삭제 처리
+    } else {
+        //InputEvent인 경우
+        const target = e.target as HTMLInputElement;
+        files = target.files;
+        if (files && files.length > 0) {
+            // uploadedFile.value = files[0].name
+        } else {
+            //취소한 경우,
+            // uploadedFile.value = '';
+        }
+    }
+    emit('update:modelValue', files)
+}
+
+onMounted(() => {
+
+})
+</script>
+
+<style lang=scss scoped>
+.file__input__wrapper{
+    width: 100%;
+    position: relative;
+    .fi__label{
+        position: relative;
+        z-index: 1;
+        display: flex;
+        width: 100%;
+        align-items: center;
+        height: 2.5rem;
+        border: 1px solid #d1d5db;
+        border-radius: 0.375rem;
+        cursor: pointer;
+        background-color: #f3f4f6;
+        @media (prefers-color-scheme: dark) {
+            &:hover {
+                background-color: #374151;
+            }
+            background-color: #4b5563;
+            border-color: #4b5563;
+            &:hover {
+                border-color: #6b7280;
+            }
+        }
+
+        span{
+            height: 100%;
+            background-color: #1f2937;
+            color: #ffffff;
+            font-size: 0.875rem;
+            padding: 0.5rem 1rem;
+            border-top-left-radius: 0.375rem;
+            border-bottom-left-radius: 0.375rem;
+            display: flex;
+            align-items: center;
+            font-weight: 500;
+        }
+
+        p{
+            padding-left: 0.5rem;
+            font-size: 0.875rem;
+            color: #6b7280;
+            @media (prefers-color-scheme: dark) {
+                color: #9ca3af;
+            }
+        }
+
+        input[type='file'] {
+            display: none;
+        }
+
+        i.init-btn {
+            font-family: 'boxicons';
+            content: 'x';
+            color: #000000;
+            font-size: 1.25rem;
+            position: absolute;
+            top: 0.5rem;
+            right: 0.5rem;
+            cursor: pointer;
+        }
+    }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <FileInput
+      :file="file"
+      @update:model-value="selectFileHandler"
+    />
   </div>
 </template>
+
+<script lang="ts" setup>
+import FileInput from '@/components/common/FileInput.vue'
+import { ref } from 'vue';
+import type { Ref } from 'vue'
+
+const file: Ref<null | File> =  ref(null);
+function selectFileHandler(files: FileList | null) {
+  if (files) {
+        file.value = files[0]
+    } else {
+        file.value = files
+    }
+    console.log('file attached - ', file.value)
+}
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
