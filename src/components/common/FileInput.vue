10c55305b6bb1d357dccb9b070b126f9e47378e8
dev-morph <112484043+dev-morph@users.noreply.github.com>
2023-12-14 03:11:49 +0900

    file component 수정 및 추가

--- src/components/common/FileInput.vue
+++ src/components/common/FileInput.vue
@@ -7,69 +7,75 @@
             @drop.prevent="fileUploadHandler"
             class="fi__label"
         >
-            <span>{{ buttonText || "파일선택" }}</span>
-            <p> {{ !file || file.name.length <= 0 ? (placeholder || "파일을 선택하거나 이곳으로 드래그 해주세요.") : file.name }} </p>
-            <input :id="uuid" type='file' title="min is " placeholder="asdfasdf" @change="(e) => fileUploadHandler(e)"/>
+            <span>{{ buttonText || '파일선택' }}</span>
+            <p>
+                {{
+                    !file || file.name.length <= 0
+                        ? placeholder || '파일을 선택하거나 이곳으로 드래그 해주세요.'
+                        : file.name
+                }}
+            </p>
+            <input
+                :id="uuid"
+                type="file"
+                title="min is "
+                placeholder="asdfasdf"
+                @change="(e) => fileUploadHandler(e)"
+            />
         </label>
-        <i v-if="fileName" @click.stop.prevent="(e) => fileUploadHandler(e)"></i>
+        <div class="init__btn" v-if="fileName" @click.stop.prevent="(e) => fileUploadHandler(e)">
+            <div class="btn__content"></div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-interface FileInputProps{
+interface FileInputProps {
     file: File | null
     buttonText?: string
     placeholder?: string
 }
-import { onMounted, ref, toRefs, computed } from 'vue';
+import { onMounted, ref, toRefs, computed } from 'vue'
 
 const props = defineProps<FileInputProps>()
-const { file, buttonText, placeholder} = toRefs(props);
-const emit = defineEmits(["update:modelValue", "test"]);
+const { file, buttonText, placeholder } = toRefs(props)
+const emit = defineEmits(['update:modelValue', 'test'])
 // const uploadedFile = ref('');
-const uuid = crypto.randomUUID();
+const uuid = crypto.randomUUID()
 
 const fileName = computed(() => {
     if (file.value) {
-        return file.value.name;
+        return file.value.name
     }
-    return null;
+    return null
 })
 
 function fileUploadHandler(e: Event) {
-    let files: FileList | null = null;
+    let files: FileList | null = null
     if (e instanceof DragEvent) {
+        //Drag
         if (e.dataTransfer) {
-            files = e.dataTransfer.files;
+            files = e.dataTransfer.files
         }
     } else if (e instanceof MouseEvent) {
-        //삭제 처리
+        //delete
     } else {
-        //InputEvent인 경우
-        const target = e.target as HTMLInputElement;
-        files = target.files;
-        if (files && files.length > 0) {
-            // uploadedFile.value = files[0].name
-        } else {
-            //취소한 경우,
-            // uploadedFile.value = '';
-        }
+        //Input Event
+        const target = e.target as HTMLInputElement
+        files = target.files
     }
     emit('update:modelValue', files)
 }
 
-onMounted(() => {
-
-})
+onMounted(() => {})
 </script>
 
-<style lang=scss scoped>
-.file__input__wrapper{
+<style lang="scss" scoped>
+.file__input__wrapper {
     width: 100%;
     position: relative;
-    .fi__label{
+    .fi__label {
         position: relative;
-        z-index: 1;
         display: flex;
         width: 100%;
         align-items: center;
@@ -89,7 +95,7 @@ onMounted(() => {
             }
         }
 
-        span{
+        span {
             height: 100%;
             background-color: #1f2937;
             color: #ffffff;
@@ -102,7 +108,7 @@ onMounted(() => {
             font-weight: 500;
         }
 
-        p{
+        p {
             padding-left: 0.5rem;
             font-size: 0.875rem;
             color: #6b7280;
@@ -114,16 +120,28 @@ onMounted(() => {
         input[type='file'] {
             display: none;
         }
+    }
+    .init__btn {
+        position: absolute;
+        width: 2.5rem;
+        height: 2.5rem;
+        top: 0;
+        right: 0;
+        color: red;
+        cursor: pointer;
 
-        i.init-btn {
-            font-family: 'boxicons';
-            content: 'x';
-            color: #000000;
-            font-size: 1.25rem;
-            position: absolute;
-            top: 0.5rem;
-            right: 0.5rem;
-            cursor: pointer;
+        .btn__content {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            &::after {
+                content: '\00d7';
+                font-size: 1.2rem;
+                color: white;
+            }
         }
     }
 }
